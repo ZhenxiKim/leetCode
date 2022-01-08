20449261dd88fa65be0866f0cb6a7359edd60e16
JeongheeKim <45592236+ZhenxiKim@users.noreply.github.com>
2022-01-08 22:19:38 +0900

    Time: 23 ms (8.84%), Space: 65.5 MB (17.87%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,24 +1,13 @@
 import java.util.*;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Map<Integer, Integer> cntMap = new HashMap<Integer, Integer>();
-        for(int i=0; i<nums.length; i++){
-            if(cntMap.get(nums[i]) == null){
-                cntMap.put(nums[i], 1);
-            }else{
-                cntMap.put(nums[i], cntMap.get(nums[i]) + 1);
-            }
-            
-            System.out.println(cntMap.get(nums[i]));
-        }
-        
-        boolean result = false;
-        
-        for(Integer key : cntMap.keySet()){
-            if(cntMap.get(key) > 1){
-                result = true;
-            }
-        }
-        return result;
+         Arrays.sort(nums);
+
+   for(int i = 0; i< nums.length-1 ; i++){
+       if(nums[i]== nums[i+1])
+           return true;
+   }
+    
+    return false;
     }
 }
