f1acc4f6d140cff10f95ae6fc2c7c3a5ec75e8c7
JeongheeKim <45592236+ZhenxiKim@users.noreply.github.com>
2023-07-15 13:15:03 +0900

    [level 1] Title: 모의고사, Time: 6.77 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,22 +1,21 @@
 def solution(answers):
-    result = []
-    score = [0, 0, 0]
-
-    std1 = [1, 2, 3, 4, 5]
+    answer = []
+    score = [0,0,0]
+    
+    std1 = [1,2,3,4,5]
     std2 = [2, 1, 2, 3, 2, 4, 2, 5]
     std3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-
+    
     for idx, answer in enumerate(answers):
-        print(idx)
         if answer == std1[idx % len(std1)]:
             score[0] += 1
-        if answer == std2[idx % len(std2)]:
+        elif answer == std2[idx % len(std2)]:
             score[1] += 1
-        if answer == std3[idx % len(std3)]:
+        elif answer == std2[idx % len(std2)]:
             score[2] += 1
-
+    
     for idx, num in enumerate(score):
         if num == max(score):
-            result.append(idx+1)
-
-    return result
+            answer.append(idx -1)
+            
+    return answer
