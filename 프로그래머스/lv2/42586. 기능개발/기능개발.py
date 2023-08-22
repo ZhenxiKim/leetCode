74f51a00f8923da0466f4d7dcbcef50f9dbfa9a8
JeongheeKim <45592236+ZhenxiKim@users.noreply.github.com>
2023-08-22 10:27:09 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,9 +1,9 @@
 def solution(progresses, speeds):
     answer = []
-    days = 0
+    day = 0
     cnt = 0
     while len(progresses) > 0:
-        if (progresses[0] + (days * speeds[0])) >= 100:
+        if progresses[0] + (day * speeds[0]) >= 100:
             progresses.pop(0)
             speeds.pop(0)
             cnt += 1
@@ -11,7 +11,7 @@ def solution(progresses, speeds):
             if cnt > 0:
                 answer.append(cnt)
                 cnt = 0
+            day += 1
 
-            days += 1
     answer.append(cnt)
     return answer
